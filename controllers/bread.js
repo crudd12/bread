92be71e0a718fe933ae1b64ffe41c20e84d1a90d
crudd12 <crudd12@gmail.com>
2024-03-07 19:52:48 -0800

    Adding delete and edit pages

--- controllers/bread.js
+++ controllers/bread.js
@@ -16,7 +16,16 @@ router.get('/new', (req, res) => {
 router.get('/:index', (req, res) => {
     const { index } = req.params  
     res.render('show', {
-        bread: Bread[index]
+        bread: Bread[index],
+        index: index 
+    })
+})
+
+router.get('/:index/edit', (req, res) => {
+    const { index } = req.params
+    res.render('edit', {
+        bread: Bread[index],
+        index
     })
 })
 
@@ -28,8 +37,29 @@ router.post('/', (req, res) => {
     } else {
         req.body.hasGluten = false
     }
+    console.log(req.body)
     Bread.push(req.body)
     res.redirect('/bread') 
+})
+
+// PUT update bread
+router.put('/:index', (req, res) => {
+    const { index } = req.params  
+    if (!req.body.image) req.body.image = 'https://houseofnasheats.com/wp-content/uploads/2022/02/French-Bread-1.jpg'
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten = true
+    } else {
+        req.body.hasGluten = false
+    }
+    Bread[index] = req.body
+    res.redirect(`/bread/${index}`)
+})
+
+// DELETE
+router.delete('/:index', (req, res) => {
+    const { index } = req.params
+    Bread.splice(index, 1)
+    res.redirect('/bread')
 })
 
 module.exports = router 
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require("react");
 const Default = require("./layouts/default");
 
-function Show({ bread }) {
+function Show({ bread, index }) {
   return (
     <Default>
       <h2>Show Page</h2>
@@ -15,6 +15,12 @@ function Show({ bread }) {
       <li>
         <a href="/bread">Go Home</a>
       </li>
+      <li>
+        <a href={`/bread/${index}/edit`}>Edit</a>
+      </li>
+      <form action={`/bread/${index}?_method=DELETE`} method='POST'>
+        <input type='submit' value='DELETE'></input>
+      </form>
     </Default>
   );
 }
